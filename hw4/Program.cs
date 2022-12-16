76a60675a462764c7d7aca7ec8f7ceed25a30954
Marat <marat3389@gmail.com>
2022-12-16 16:37:07 +0300

    Сделал 4 задание

---
+++ hw4/Program.cs
@@ -0,0 +1,9 @@
+﻿int a = Convert.ToInt32(Console.ReadLine());
+int i = 2;
+Console.Write("Все четные числа от 0 до " + a + ": ");
+while (i <= a) {
+    if (i % 2 == 0) {
+        Console.Write(i + ", ");
+    }
+    i++;
+}
