4c939781f5040aab26e6c30bbc0c926142dc0a67
Marat <marat3389@gmail.com>
2022-12-16 16:08:03 +0300

    Сделал второе задание

--- hw2/Program.cs
+++ hw2/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+if(a > b) 
+{
+    if (a > c) 
+    {    
+        Console.Write("max = " + a);
+    }    
+}
+
+if(b > a) 
+{
+    if (b > c) 
+    {    
+        Console.Write("max = " + b);
+    }    
+}
+
+if(c > a) 
+{
+    if (c > b) 
+    {    
+        Console.Write("max = " + c);
+    }     
+}
