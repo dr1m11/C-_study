edd662ad38aad8ebb50671e157c568ebc39b4c21
Marat <marat3389@gmail.com>
2022-12-16 15:15:52 +0300

    Добавил первое дз

---
+++ hw1/Program.cs
@@ -0,0 +1,14 @@
+﻿string a = Console.ReadLine();
+string b = Console.ReadLine();
+if (a > b) {
+    max = a;
+    min = b;
+}
+else {
+    max = b;
+    min = c;
+}
+Console.Write("max =");
+Console.WriteLine(max);
+Console.Write("min =");
+Console.WriteLine(min);
